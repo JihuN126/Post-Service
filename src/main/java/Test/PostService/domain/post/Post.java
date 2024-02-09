56551161aa0f7832d87232fc9c62b03964d3074d
JihuN126 <dbwlgns1208@naver.com>
2024-02-09 17:46:54 +0900

    commit

--- src/main/java/Test/PostService/domain/post/Post.java
+++ src/main/java/Test/PostService/domain/post/Post.java
@@ -1,11 +1,17 @@
 package Test.PostService.domain.post;
 import lombok.Data;
+
+import java.util.ArrayList;
+
 @Data
 public class Post {
     private long id;
     private String title;
     private String content;
     private String writer;
+    private int commentCount;
+    public ArrayList<String> commentContent = new ArrayList<>();
+    public ArrayList<String> commentWriter = new ArrayList<>();
     public Post() {
     }
     public Post(String title, String content, String writer) {
@@ -16,5 +22,11 @@ public class Post {
     public Post(String title, String content) {
         this.title = title;
         this.content = content;
+    }
+    public void setCommentContent(String commentContent) {
+        this.commentContent.add(commentContent);
+    }
+    public void setCommentWriter(String commentWriter) {
+        this.commentContent.add(commentWriter);
     }
 }
--- src/main/java/Test/PostService/web/basic/BasicPostController.java
+++ src/main/java/Test/PostService/web/basic/BasicPostController.java
@@ -1,18 +1,17 @@
 package Test.PostService.web.basic;
-
 import Test.PostService.domain.post.Post;
 import Test.PostService.domain.post.PostRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
 @Controller
 @RequestMapping("/basic/posts")
 @RequiredArgsConstructor
+@Slf4j
 public class BasicPostController {
     private final PostRepository postRepository;
     @GetMapping
@@ -38,11 +37,13 @@ public class BasicPostController {
     @GetMapping("/{postId}")
     public String post(@PathVariable(name="postId") long postId, Model model) {
         Post post = postRepository.findById(postId);
+        System.out.println("BasicPostController.post");
         model.addAttribute("post",post);
         return "basic/post";
     }
     @GetMapping("/{postId}/edit")
     public String editForm(@PathVariable(name="postId") long postId, Model model) {
+        System.out.println("BasicPostController.editForm");
         Post post = postRepository.findById(postId);
         model.addAttribute("post",post);
         return "basic/editForm";
@@ -53,13 +54,45 @@ public class BasicPostController {
                        @RequestParam String postContent,
                        Model model) {
         Post post = new Post(postTitle, postContent);
+        System.out.println("BasicPostController.edit");
         postRepository.update(postId,post);
         model.addAttribute("post",post);
         return "redirect:/basic/posts/{postId}";
     }
+    @GetMapping("/{postId}/commentslist")
+    public String commentList(@PathVariable(name="postId") long postId, Model model) {
+        Post post = postRepository.findById(postId);
+        model.addAttribute("post",post);
+        System.out.println("BasicPostController.commentList");
+        return "basic/commentList";
+    }
+    @GetMapping("/{postId}/comments")
+    public String commentForm(@PathVariable(name="postId") long postId, Model model) {
+        Post post = postRepository.findById(postId);
+        System.out.println("BasicPostController.commentForm");
+        model.addAttribute("post",post);
+        return "basic/commentForm";
+    }
+    @PostMapping("/{postId}/comments")
+    public String commentWrite(@PathVariable(name="postId") long postId,
+                          @RequestParam String commentContent,
+                          @RequestParam String commentWriter,
+                          Model model) {
+        System.out.println("BasicPostController.comment");
+        Post post = postRepository.findById(postId);
+        post.setCommentContent(commentContent);
+        post.setCommentWriter(commentWriter);
+        model.addAttribute("post",post);
+        System.out.println(post.commentContent.size());
+        System.out.println(post.commentWriter.size());
+//        for(int i=0;i<post.commentContent.size();i++) {
+//            System.out.println(post.commentContent.get(i));
+//        }
+        return "redirect:/basic/posts/{postId}/commentslist";
+    }
     @PostConstruct
     public void init() {
-        postRepository.save(new Post("안녕하세요", "asasasasasasas", "yjh"));
-        postRepository.save(new Post("안녕하세요222", "asasasasasasa232323s", "yjhdsd"));
+        postRepository.save(new Post("Title1", "Content1", "Writer1"));
+        postRepository.save(new Post("Title2", "Content2", "Writer2"));
     }
 }
